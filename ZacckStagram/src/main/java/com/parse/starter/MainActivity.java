0d07f4c1145f82bc270c8098e6821c25c52ce678
zacck <coderv63@gmail.com>
2016-02-10 08:25:10 +0200

    Added User Interface for sign up and Login

--- ZacckStagram/src/main/java/com/parse/starter/MainActivity.java
+++ ZacckStagram/src/main/java/com/parse/starter/MainActivity.java
@@ -17,9 +17,17 @@ import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.GetCallback;
@@ -36,33 +44,164 @@ import com.parse.SignUpCallback;
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    ParseAnalytics.trackAppOpenedInBackground(getIntent());
+    EditText etName;
+    EditText etPassword;
+    TextView changeSignupModetv;
+    Button btSignUp;
+    boolean signUpModeActive = true;
+    RelativeLayout base;
+    ImageView ivLogo;
 
 
-      //working with parseusers
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+        etName = (EditText) findViewById(R.id.etUserName);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        //btLogin = (Button) findViewById(R.id.btlogin);
+        btSignUp = (Button) findViewById(R.id.btsignup);
+        changeSignupModetv = (TextView)findViewById(R.id.tvLogin);
+        base = (RelativeLayout)findViewById(R.id.relativeLayout);
+        ivLogo = (ImageView)findViewById(R.id.ivlogo);
+
+        base.setOnClickListener(this);
+        ivLogo.setOnClickListener(this);
+        changeSignupModetv.setOnClickListener(this);
+
+        etPassword.setOnKeyListener(this);
+
+
+    }
+
+
+    public void alert(String Message) {
+        Toast.makeText(getApplicationContext(), Message, Toast.LENGTH_LONG).show();
+    }
+
+    public void userSignup(View view)
+    {
+        String uname = etName.getText().toString();
+        String pword = etPassword.getText().toString();
+        if(signUpModeActive)
+        {
+            if (uname.isEmpty() || pword.isEmpty()) {
+                alert("Please input both username and password fields");
+            } else {
+                ParseUser mParseUser = new ParseUser();
+                mParseUser.setUsername(uname);
+                mParseUser.setPassword(pword);
+
+
+                mParseUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Log.i("SignUp", "Succesfull");
+                        } else {
+                            alert(e.getMessage());
+
+                        }
+                    }
+                });
+
+            }
+
+        }
+        else
+        {
+            if (uname.isEmpty() || pword.isEmpty()) {
+                alert("Please input both username and password fields");
+            } else {
+                ParseUser.logInInBackground(uname, pword, new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Log.i("Login", "Success");
+                        } else {
+                            Log.i("Login", "unsuccessful");
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        }
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.tvLogin:
+                if(signUpModeActive)
+                {
+                    signUpModeActive = false;
+                    changeSignupModetv.setText("Sign Up");
+                    btSignUp.setText("Log In");
+                }
+                else
+                {
+                    signUpModeActive = true;
+                    changeSignupModetv.setText("Log In");
+                    btSignUp.setText("Sign Up");
+                }
+                break;
+            case R.id.relativeLayout:
+                InputMethodManager mImm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                mImm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+                break;
+            case R.id.ivlogo:
+                InputMethodManager mImm2 = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                mImm2.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),0);
+                break;
+
+        }
+    }
+
+    @Override
+    public boolean onKey(View view, int code, KeyEvent keyEvent) {
+        //check if enter key has been pressed
+        if(code == keyEvent.KEYCODE_ENTER && keyEvent.getAction() == keyEvent.ACTION_DOWN)
+        {
+            userSignup(view);
+        }
+        return false;
+    }
+    //working with parseusers
 
       /*Logging Users In
-      ParseUser.logInInBackground("Zacck", "mUserPass" ,new LogInCallback() {
-          @Override
-          public void done(ParseUser user, ParseException e) {
-              if(user != null)
-              {
-                  Log.i("Login", "Success");
-              }
-              else
-              {
-                  Log.i("Login", "unsuccessful");
-                  e.printStackTrace();
-              }
-          }
-      });
-      */
+      //signing up
+
+
+
       if(ParseUser.getCurrentUser() != null)
       {
           Log.i("LoggedIn", "UserLogged In");
@@ -73,53 +212,9 @@ public class MainActivity extends AppCompatActivity {
       {
           Log.i("LoggediIN", "User Is not logged in");
       }
-
-
-
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
+      */
 
      /* Advanced Querying with Parse
-     ParseUser mParseUser = new ParseUser();
-    mParseUser.setUsername("Zacck");
-    mParseUser.setPassword("mUserPass");
-
-
-    mParseUser.signUpInBackground(new SignUpCallback() {
-        @Override
-        public void done(ParseException e) {
-            if(e == null)
-            {
-                Log.i("SignUp", "Succesfull");
-            }
-            else
-            {
-                Log.i("exception", e.toString());
-
-            }
-        }
-    });
 
       //ParseQuery<ParseObject> mScoreQuery = ParseQuery.getQuery("Score");
       //get specific objects and update them
@@ -219,8 +314,6 @@ public class MainActivity extends AppCompatActivity {
           }
       });
       */
-
-
 
 
 }
